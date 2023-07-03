db0482121226ec7d4a528a684d31f68fd0a3da42
simhadrimallaiah1998 <simhadrimallaiah1998@gmail.com>
2023-07-03 13:07:50 +0530

    cors setting

--- nodechat/server.js
+++ nodechat/server.js
@@ -1,24 +1,25 @@
-const express=require("express")
-const userRoutes=require("./src/user/router")
-const app=express()
-const port=4000
+const express = require("express");
+const userRoutes = require("./src/user/router");
+const app = express();
+const port = 4000;
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to the Express Server")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Server");
+});
 
-
-
-app.use('/api/v1/user',userRoutes)
-app.listen(port,()=>{
-    console.log(`server has been started at port ${port}`)
-})
+app.use("/api/v1/user", userRoutes);
+app.listen(port, () => {
+  console.log(`server has been started at port ${port}`);
+});
