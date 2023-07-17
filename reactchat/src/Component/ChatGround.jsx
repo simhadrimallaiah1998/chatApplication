498e2eef3333ef3c707c94bbe70047a45a9fd8de
simhadrimallaiah1998 <simhadrimallaiah1998@gmail.com>
2023-07-17 23:08:56 +0530

    styling chat

--- reactchat/src/Component/ChatGround.jsx
+++ reactchat/src/Component/ChatGround.jsx
@@ -63,15 +63,17 @@ const ChatGround = () => {
     document.getElementById("chatBox").value = "";
     document.getElementById("chatBox2").value = "";
 
-    const [res, err] = await api.personToPerson({
-      sender_id: loginId,
-      receiver_id: receiver,
-      chat: mess,
-    });
-    setMess((document.getElementById("chatBox").value = ""));
-    setMess((document.getElementById("chatBox2").value = ""));
-    if (err) throw err;
-    console.log(res.data);
+    if (mess.length > 0) {
+      const [res, err] = await api.personToPerson({
+        sender_id: loginId,
+        receiver_id: receiver,
+        chat: mess,
+      });
+      setMess((document.getElementById("chatBox").value = ""));
+      setMess((document.getElementById("chatBox2").value = ""));
+      if (err) throw err;
+      console.log(res.data);
+    }
 
     async function getPersonalChat() {
       const [res, err] = await api.getPersonalChat({
@@ -89,13 +91,13 @@ const ChatGround = () => {
   function getfuntion(e) {
     if (e.sender_id === loginId) {
       return (
-        <div className="text-right text-white   bg-transparent hover:border-white hover:border px-2 py-2 rounded-lg  m-1 ">
+        <div className="text-right font-bold w-full  text-white hover:p-4 hover:bg-[url('https://png.pngtree.com/background/20210709/original/pngtree-shading-background-abstract-colorful-background-colorful-art-picture-image_938007.jpg')] hover:opacity-100 hover:text-black hover:font-extrabold  hover:bg-cover   bg-black opacity-80 hover:border-white hover:border px-2 py-2 rounded-lg  m-1 ">
           {e.chat}
         </div>
       );
     }
     return (
-      <h1 className="text-left text-white bg-transparent hover:border-white hover:border px-2 py-2 rounded-lg m-2 ">
+      <h1 className="text-left font-bold w-full  text-white hover:p-4 hover:bg-[url('https://png.pngtree.com/background/20210709/original/pngtree-shading-background-abstract-colorful-background-colorful-art-picture-image_938007.jpg')] hover:opacity-100 hover:text-black hover:font-extrabold  hover:bg-cover   bg-black opacity-80 hover:border-white hover:border px-2 py-2 rounded-lg  m-1 ">
         {e.chat}
       </h1>
     );
@@ -149,10 +151,7 @@ const ChatGround = () => {
               chat.map((e) => getfuntion(e))
             ) : (
               <h1 className="text-white text-center mt-10">
-                Dear {loginId} <br />
-                You Haven't Started Chatted with {receiver} before...!
-                <br />
-                PLEASE START YOUR CHAT
+                DEAR {loginId} PLEASE START YOUR CHAT {receiver}
               </h1>
             )}
           </div>
@@ -183,23 +182,25 @@ const ChatGround = () => {
 
             <div className="flex flex-row overflow-scroll">
               {SignId.map((e) => (
-                <div className=" w-[95%] font-extrabold hover:bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAtdv5X5MEqVnjNLyFaXQOHnN8VE0V539Ydw&usqp=CAU')] hover:bg-cover hover:w-full hover:text-white hover:px-10 border-4 border-blue-950 rounded-lg  shadow-xl py-2 text-blue-950 m-1 text-start px-5">
+                <div className=" w-[95%] font-extrabold hover:bg-[url('https://wallpaperaccess.com/full/692085.jpg')] hover:bg-cover hover:w-full hover:text-black hover:px-10 border-4 border-blue-950 rounded-lg  shadow-xl py-2 text-blue-950 m-1 text-start px-5">
                   <button type="button" onClick={() => handleUserId(e)} key={e}>
                     {e}
                   </button>
                 </div>
               ))}
             </div>
-            <button
-              className="border-4  py-2 text-white bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAtdv5X5MEqVnjNLyFaXQOHnN8VE0V539Ydw&usqp=CAU')]  border-blue-950 w-full  font-extrabold rounded-3xl "
-              onClick={handleDelete}
-            >
-              Delete Chat with {receiver}
-            </button>
+            {chat.length > 0 && (
+              <button
+                className="border-4  py-2 text-black bg-[url('https://wallpaperaccess.com/full/692085.jpg')] bg-cover   border-blue-950 w-full  font-extrabold rounded-3xl"
+                onClick={handleDelete}
+              >
+                Delete Chat with {receiver}
+              </button>
+            )}
           </div>
 
-          <div className="h-[75%] relative overflow-scroll bg-[url('https://i.pinimg.com/originals/4d/fc/98/4dfc98fe97aec74439cc1701fbf599dc.jpg')]   bg-cover">
-            <div className="h-[10%] w-full py-2  bg-gradient-to-t from-transparent via-blue-800 to-transparent px-4 text-sm   text-center font-extrabold text-white">
+          <div className="h-[75%] relative overflow-scroll bg-[url('https://w0.peakpx.com/wallpaper/629/15/HD-wallpaper-gentlemen-nani-nivetha.jpg')]   bg-cover">
+            <div className="h-[10%] w-full py-2   px-4 text-sm   text-center font-extrabold text-black">
               Hey {loginId} You are right now chatting with {receiver}
             </div>
 
@@ -207,12 +208,13 @@ const ChatGround = () => {
               {chat.length > 0 ? (
                 chat.map((e) => getfuntion(e))
               ) : (
-                <h1 className="text-white text-center mt-10">
-                  Dear {loginId} <br />
-                  You Haven't Started Chatted with {receiver} before...!
-                  <br />
-                  PLEASE START YOUR CHAT
-                </h1>
+                <div className=" bg-white rounded-xl hover:bg-[url('https://png.pngtree.com/background/20210709/original/pngtree-shading-background-abstract-colorful-background-colorful-art-picture-image_938007.jpg')]  bg-cover opacity-80 ">
+                  {" "}
+                  <h1 className="text-black  font-extrabold text-center mt-10">
+                    DEAR {loginId} <br />
+                    PLEASE START YOUR CHAT {receiver}
+                  </h1>
+                </div>
               )}
             </div>
           </div>
@@ -222,7 +224,7 @@ const ChatGround = () => {
                 type="text"
                 id="chatBox"
                 className="lg:w-[94%] font-extrabold text-black border-none w-[80%] py-2 bg-gray-100 px-4"
-                placeholder="please enter your text"
+                placeholder="Enter your text........"
                 onChange={handleChat}
               />
               <button
